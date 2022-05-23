19cf755ca5356c7c5095aa1276a99fe7ac123aef
Tran Duc Anh Dang <anhdang.contact@gmail.com>
2022-05-23 20:25:10 +1000

    1.0

--- assign2/session.php
+++ assign2/session.php
@@ -3,7 +3,7 @@
 session_start();
 //Check whether the session variable SESS_MEMBER_ID is present or not
 if (!isset($_SESSION['user_id']) || (trim($_SESSION['user_id']) == '')) {
-    header("location: index.php");
+    header("location: manage.php");
     exit();
 }
 $session_id=$_SESSION['user_id'];

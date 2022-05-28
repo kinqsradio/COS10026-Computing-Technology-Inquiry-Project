e5c656d43f322a3045e4a9bbdc1632c16bea13c7
Tommy <quangthanhtommy124@gmail.com>
2022-05-28 15:38:32 +1000

    1.0
    T vua sua lai cai session.php. Bay gio khi ma minh nhap manage.php thi se chay ra admin.php

--- assign2/session.php
+++ assign2/session.php
@@ -3,7 +3,7 @@
 session_start();
 //Check whether the session variable SESS_MEMBER_ID is present or not
 if (!isset($_SESSION['user_id']) || (trim($_SESSION['user_id']) == '')) {
-    header("location: manage.php");
+    header("location: admin.php");
     exit();
 }
 $session_id=$_SESSION['user_id'];
